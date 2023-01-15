6bdf59dc74c2774c0e1800d9df4c4f64e20f3335
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-15 14:33:41 +0300

    Задание 2

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -10,13 +10,10 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 }
 
-int a1 = 15, b1 = 21, c1 = 39, 
-    a2 = 12, b2 = 22, c2 = 33,
-    a3 = 13, b3 = 23, c3 = 33;
-
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+//               0   1   2   3   4   5   6   7   8
+int[] array = { 11, 21, 13, 14, 63, 14, 72, 97, 16 };
+int max = Max(Max(array[0], array[1], array[2]),
+              Max(array[3], array[4], array[5]),
+              Max(array[6], array[7], array[8]));
 
 Console.WriteLine(max);
