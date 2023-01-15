9baf137a4d82f9ce143ee68e2cf43eda6b39f2f9
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-15 14:56:31 +0300

    Задание 4

---
+++ Exercise04/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] arrayRandom)
+{
+    int length = arrayRandom.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arrayRandom[index] = new Random().Next(0, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] arrayPrint)
+{
+    int length = arrayPrint.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write(arrayPrint[index] + " ");
+        index++;
+    }
+}
+
+int IndexOf(int[] arraySearchPoition, int find)
+{
+    int length = arraySearchPoition.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < length)
+    {
+        if (arraySearchPoition[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
