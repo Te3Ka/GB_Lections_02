70a545b651f83664191877925ca36bba71bec3c4
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-15 14:40:01 +0300

    Задание 3

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -13,7 +13,7 @@ int Max(int arg1, int arg2, int arg3)
 //               0   1   2   3   4   5   6   7   8
 int[] array = { 11, 21, 13, 14, 63, 14, 72, 97, 16 };
 int max = Max(Max(array[0], array[1], array[2]),
-              Max(array[3], array[4], array[5]),
-              Max(array[6], array[7], array[8]));
+          Max(array[3], array[4], array[5]),
+          Max(array[6], array[7], array[8]));
 
 Console.WriteLine(max);
---
+++ Exercise03/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+int n = array.Length;
+int find = 0;
+
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine("Найдено! Позиция = " + i);
+        break;
+    }
+    i++;
+}
